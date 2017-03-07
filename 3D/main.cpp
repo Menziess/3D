5b392006cda04e5d8518a3da32daf2dc6efdd014
Menzies <stefan_schenk@hotmail.com>
2017-03-07 17:23:53 +0100

    Added mesh class to draw a triangle

--- 3D/main.cpp
+++ 3D/main.cpp
@@ -2,17 +2,27 @@
 #include <GL\glew.h>
 #include "display.h"
 #include "shader.h"
+#include "mesh.h"
 
-int main() 
+int main()
 {
 	Display display{ 800, 600, "test" };
 
 	Shader shader("./res/basicShader");
 
+	Vertex vertices[] = { Vertex(glm::vec3(-.5, -0.5, 0)),
+						  Vertex(glm::vec3(0, 0.5, 0)),
+						  Vertex(glm::vec3(0.5, -0.5, 0)), };
+
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+
 	while (!display.IsClosed())
 	{
 		display.Clear(0.1f, 0.3f, 0.4f, 1.0f);
+
 		shader.Bind();
+
+		mesh.Draw();
 
 		display.Update();
 	}
